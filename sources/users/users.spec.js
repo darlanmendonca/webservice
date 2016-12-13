66ef70c0c7acb42a3bab74e3f7f8b378ff664a79
Darlan Mendonça <darlanmendonca@gmail.com>
2016-12-13 14:49:52 -0200

    full specs to users.authenticate()

--- sources/users/users.spec.js
+++ sources/users/users.spec.js
@@ -14,9 +14,47 @@ describe('Users', () => {
         .field('email', user.email)
         .field('password', user.invalidPassword)
         .end((err, res) => {
-          expect(res).to.have.status(401)
           expect(res).to.be.json
+          expect(res).to.have.status(401)
           expect(res.body).to.have.property('message', 'invalid credentials')
+          done()
+        })
+    })
+
+    it('require email', (done) => {
+      request(webservice)
+        .post('/users/authenticate')
+        .field('email', user.email)
+        .end((err, res) => {
+          expect(res).to.be.json
+          expect(res).to.have.status(401)
+          expect(res.body).to.have.property('message', 'invalid credentials')
+          done()
+        })
+    })
+
+    it('require password', (done) => {
+      request(webservice)
+        .post('/users/authenticate')
+        .field('password', user.password)
+        .end((err, res) => {
+          expect(res).to.be.json
+          expect(res).to.have.status(401)
+          expect(res.body).to.have.property('message', 'invalid credentials')
+          done()
+        })
+    })
+
+    it('valid credentials', (done) => {
+      request(webservice)
+        .post('/users/authenticate')
+        .field('email', user.email)
+        .field('password', user.password)
+        .end((err, res) => {
+          expect(res).to.be.json
+          expect(res).to.have.status(200)
+          expect(res.body).to.have.property('id')
+          expect(res.body).to.have.property('token')
           done()
         })
     })
