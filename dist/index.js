6ca09f8595d899a914e756890adcca1784f8abc6
Darlan Mendonça <darlanmendonca@gmail.com>
2017-01-12 10:26:52 -0200

    add built do pretest

--- dist/index.js
+++ dist/index.js
@@ -26,6 +26,10 @@ var _multer2 = _interopRequireDefault(_multer);
 
 var _bodyParser = require('body-parser');
 
+var _cors = require('cors');
+
+var _cors2 = _interopRequireDefault(_cors);
+
 var _compression = require('compression');
 
 var _compression2 = _interopRequireDefault(_compression);
@@ -40,7 +44,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 var webservice = (0, _express2.default)();
 
-webservice.use((0, _compression2.default)()).use((0, _methodOverride2.default)()).use((0, _multer2.default)().array()).use((0, _bodyParser.urlencoded)({ extended: true })).use((0, _bodyParser.json)()).use(_filterHelper2.default).use(_router2.default);
+webservice.use((0, _cors2.default)()).use((0, _compression2.default)()).use((0, _methodOverride2.default)()).use((0, _multer2.default)().array()).use((0, _bodyParser.urlencoded)({ extended: true })).use((0, _bodyParser.json)()).use(_filterHelper2.default).use(_router2.default);
 
 _mongoose2.default.Promise = _bluebird2.default;
 
