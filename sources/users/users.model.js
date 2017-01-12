e09a187912a25c205faf5553cddb2c78bf9d24fa
Darlan Mendonça <darlanmendonca@gmail.com>
2017-01-12 14:13:07 -0200

    set password as optional in users model

--- sources/users/users.model.js
+++ sources/users/users.model.js
@@ -7,7 +7,7 @@ const schema = new mongoose.Schema({
   firstname: {type: String, trim: true, required: true},
   lastname: {type: String, trim: true, required: true},
   email: {type: String, trim: true, required: true, unique: true},
-  password: {type: String, required: true, set: encode.md5, private: true},
+  password: {type: String, set: encode.md5, private: true},
   active: {type: Boolean, default: true},
   __v: {type: Number, private: true},
 })
